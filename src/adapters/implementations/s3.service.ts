f8220700079bd2d27aafa14bc6992e197af5095d
Henrique Leite <contact.henriqueleite42@gmail.com>
2023-09-26 22:08:09 -0300

    Add S3 Adapter (#22)

---
+++ src/adapters/implementations/s3.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@nestjs/common';
+import { FileAdapter, GetInput, SaveInput } from '../file';
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
+import { createReadStream, writeFileSync } from 'fs';
+import { join } from 'path';
+import { Readable } from 'stream';
+
+@Injectable()
+export class S3Adapter implements FileAdapter {
+  client: S3Client;
+
+  constructor() {
+    if (process.env['NODE_ENV'] === 'production') {
+      this.client = new S3Client();
+    } else {
+      this.client = {} as S3Client;
+    }
+  }
+
+  async save({ folder, filePath, file, metadata }: SaveInput) {
+    let path: string;
+
+    if (process.env['NODE_ENV'] === 'production') {
+      await this.client.send(
+        new PutObjectCommand({
+          Bucket: folder,
+          Key: filePath,
+          Body: file,
+          Metadata: metadata,
+        }),
+      );
+
+      path = `${folder}/${filePath}`;
+    } else {
+      path = join(__dirname, 'tmp', 'assets', filePath);
+
+      writeFileSync(path, file);
+    }
+
+    return path;
+  }
+
+  async getStream({ folder, filePath }: GetInput): Promise<Readable> {
+    if (process.env['NODE_ENV'] === 'production') {
+      const x = await this.client.send(
+        new GetObjectCommand({
+          Bucket: folder,
+          Key: filePath,
+        }),
+      );
+
+      return x.Body! as Readable;
+    } else {
+      return createReadStream(join(__dirname, 'tmp', 'assets', filePath));
+    }
+  }
+}
