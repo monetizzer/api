72fa8e251e3ac7c1f9972d47a6e4cfe2aa502f55
Henrique Leite <contact.henriqueleite42@gmail.com>
2023-09-28 00:59:29 -0300

    Add Notification Repository (#31)

---
+++ src/repositories/mongodb/notification/notification-repository.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { NotificationRepositoryService } from './notification-repository.service';
+import { MongoDBModule } from '..';
+import { UIDAdapter } from 'src/adapters/implementations/uid.service';
+
+@Module({
+  imports: [MongoDBModule.forFeature(['notifications'])],
+  providers: [NotificationRepositoryService, UIDAdapter],
+  exports: [NotificationRepositoryService],
+})
+export class NotificationRepositoryModule {}
