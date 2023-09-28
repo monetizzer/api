91435f505e2b567a45b9c874f2ee37658d153c30
Henrique Leite <contact.henriqueleite42@gmail.com>
2023-09-28 12:32:54 -0300

    Add Store Create Route

--- src/delivery/validators/internal.ts
+++ src/delivery/validators/internal.ts
@@ -47,3 +47,49 @@ export function IsMagicLinkCode(validationOptions: ValidationOptions = {}) {
     });
   };
 }
+
+export function IsUsername(validationOptions: ValidationOptions = {}) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'isUsername',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [],
+      options: {
+        message: `${propertyName} must be a valid username`,
+        ...validationOptions,
+      },
+      validator: {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        validate(value: any, _args: ValidationArguments) {
+          return (
+            typeof value === 'string' && /^[a-zA-Z0-9_-]{3,16}$/.test(value)
+          );
+        },
+      },
+    });
+  };
+}
+
+export function IsHEXColor(validationOptions: ValidationOptions = {}) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'isHEXColor',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [],
+      options: {
+        message: `${propertyName} must be a valid HEX color`,
+        ...validationOptions,
+      },
+      validator: {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        validate(value: any, _args: ValidationArguments) {
+          return typeof value === 'string' && /^#[A-Fa-f0-9]{6}$/.test(value);
+        },
+      },
+    });
+  };
+}
---
+++ src/usecases/store/store.service.ts
@@ -0,0 +1,98 @@
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { S3Adapter } from 'src/adapters/implementations/s3.service';
+import {
+  CreateStoreInput,
+  StoreUseCase,
+  UpdateStoreInput,
+} from 'src/models/store';
+import { AccountRepositoryService } from 'src/repositories/mongodb/account/account-repository.service';
+import { DocumentRepositoryService } from 'src/repositories/mongodb/document/document-repository.service';
+import { StoreRepositoryService } from 'src/repositories/mongodb/store/store-repository.service';
+import { DocumentStatusEnum } from 'src/types/enums/document-status';
+
+@Injectable()
+export class StoreService implements StoreUseCase {
+  constructor(
+    @Inject(StoreRepositoryService)
+    private readonly storeRepository: StoreRepositoryService,
+    @Inject(DocumentRepositoryService)
+    private readonly documentRepository: DocumentRepositoryService,
+    @Inject(AccountRepositoryService)
+    private readonly accountRepository: AccountRepositoryService,
+    private readonly fileAdapter: S3Adapter,
+  ) {}
+
+  async create({
+    accountId,
+    username,
+    avatar,
+    banner,
+    ...i
+  }: CreateStoreInput): Promise<void> {
+    const [store, document, storeUsername] = await Promise.all([
+      this.storeRepository.getByAccountId({ accountId }),
+      this.documentRepository.getByAccountId({ accountId }),
+      this.storeRepository.getByUsername({ username }),
+    ]);
+
+    if (store) {
+      throw new HttpException('You already has an store', HttpStatus.CONFLICT);
+    }
+
+    if (document?.status !== DocumentStatusEnum.AA) {
+      throw new HttpException(
+        'You must validate your documents first',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    if (storeUsername) {
+      throw new HttpException(
+        `There already exists an store with the username "${username}"`,
+        HttpStatus.CONFLICT,
+      );
+    }
+
+    const promises: Array<Promise<any>> = [
+      this.accountRepository.updateUsername({
+        accountId,
+        username,
+      }),
+      this.storeRepository.create({
+        ...i,
+        accountId,
+        username,
+        bannerUrl: banner
+          ? `${process.env['PUBLIC_FILES_URL']}/stores/banners/${accountId}.jpeg`
+          : undefined,
+        avatarUrl: avatar
+          ? `${process.env['PUBLIC_FILES_URL']}/stores/avatars/${accountId}.jpeg`
+          : undefined,
+      }),
+    ];
+
+    if (avatar) {
+      promises.push(
+        this.fileAdapter.save({
+          folder: 'stores',
+          filePath: `avatars/${accountId}.jpeg`,
+          file: avatar,
+        }),
+      );
+    }
+
+    if (banner) {
+      promises.push(
+        this.fileAdapter.save({
+          folder: 'stores',
+          filePath: `banners/${accountId}.jpeg`,
+          file: banner,
+        }),
+      );
+    }
+
+    await Promise.all(promises);
+  }
+
+  async update(i: UpdateStoreInput): Promise<void> {}
+}
