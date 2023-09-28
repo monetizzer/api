d94ff552dade4fbe1c6c48c0f2421a469e6050e3
Henrique Leite <contact.henriqueleite42@gmail.com>
2023-09-28 00:44:29 -0300

    Add Notification Model

---
+++ src/models/notification.ts
@@ -0,0 +1,50 @@
+import { PlatformEnum } from 'src/types/enums/platform';
+
+export interface NotificationEntity {
+  notificationId: string;
+  accountId: string;
+  platform: PlatformEnum;
+  title: string;
+  description: string;
+  data?: Record<string, string>;
+  createdAt: Date;
+}
+
+/**
+ *
+ *
+ * Repository
+ *
+ *
+ */
+
+export interface SaveInput {
+  accountId: string;
+  platform: PlatformEnum;
+  title: string;
+  description: string;
+  data?: Record<string, string>;
+}
+
+export interface TermsRepository {
+  save: (i: SaveInput) => Promise<void>;
+}
+
+/**
+ *
+ *
+ * Usecase
+ *
+ *
+ */
+
+export interface SendNotificationInput {
+  accountId: string;
+  title: string;
+  description: string;
+  data?: Record<string, string>;
+}
+
+export interface TermsUseCase {
+  sendNotification: (i: SendNotificationInput) => Promise<void>;
+}
