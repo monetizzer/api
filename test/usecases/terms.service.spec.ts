b1cba76132f7f602ab27a20502a0834142aaa8a9
Henrique Leite <contact.henriqueleite42@gmail.com>
2023-10-04 21:35:43 -0300

    Update Test Files (#57)

--- test/usecases/terms.service.spec.ts
+++ test/usecases/terms.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TermsService } from '../../src/usecases/terms/terms.service';
 
 describe('TermsService', () => {
-  let service: TermsService;
+	let service: TermsService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TermsService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [TermsService],
+		}).compile();
 
-    service = module.get<TermsService>(TermsService);
-  });
+		service = module.get<TermsService>(TermsService);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
 });
