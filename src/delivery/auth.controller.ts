8c10b482d613ff28e7a0fbe7be77e4b5bc32c020
Henrique Leite <contact.henriqueleite42@gmail.com>
2023-10-06 00:11:44 -0300

    Add Auth Cookie (#69)

--- src/delivery/auth.controller.ts
+++ src/delivery/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { AccountService } from 'src/usecases/account/account.service';
 import {
 	AcceptTermsDto,
@@ -8,17 +8,23 @@ import {
 } from './dtos/auth';
 import { AuthGuard } from './guards/auth.guard';
 import { AccountId } from './decorators/account-id';
+import { Response } from 'express';
+import { AuthOutput } from 'src/models/account';
 
 @Controller('')
 export class AuthController {
 	constructor(private readonly accountService: AccountService) {}
 
 	@Post('/auth/discord')
-	createFromDiscordOauth(
+	async createFromDiscordOauth(
 		@Body()
 		body: CreateFromDiscordOauthDto,
+		@Res({ passthrough: true })
+		res: Response,
 	) {
-		return this.accountService.createFromDiscordOauth(body);
+		const auth = await this.accountService.createFromDiscordOauth(body);
+
+		this.setCookies(auth, res);
 	}
 
 	@Post('/auth/magic-link/gen-email')
@@ -30,11 +36,15 @@ export class AuthController {
 	}
 
 	@Post('/auth/magic-link')
-	createFromMagicLink(
+	async createFromMagicLink(
 		@Body()
 		body: CreateFromMagicLinkDto,
+		@Res({ passthrough: true })
+		res: Response,
 	) {
-		return this.accountService.createFromMagicLink(body);
+		const auth = await this.accountService.createFromMagicLink(body);
+
+		this.setCookies(auth, res);
 	}
 
 	@Post('/terms/accept')
@@ -59,6 +69,15 @@ export class AuthController {
 	) {
 		return this.accountService.iam({
 			accountId,
+		});
+	}
+
+	// Private
+
+	private setCookies({ accessToken }: AuthOutput, res: Response) {
+		res.cookie('access-token', accessToken, {
+			httpOnly: true,
+			secure: process.env['NODE_ENV'] === 'production',
 		});
 	}
 }
