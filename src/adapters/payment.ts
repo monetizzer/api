047cd840184d6dcdaee9dd284b96d0cb52ac4029
Henrique Leite <contact.henriqueleite42@gmail.com>
2023-10-07 15:04:47 -0300

    Add Payment Adapter (#72)

---
+++ src/adapters/payment.ts
@@ -0,0 +1,19 @@
+export interface GenPixInput {
+	saleId: string;
+	value: number;
+}
+
+export interface GenPixOutput {
+	code: string;
+	qrCodeBase64: string;
+}
+
+export interface RefundInput {
+	saleId: string;
+}
+
+export interface PaymentAdapter {
+	genPix: (i: GenPixInput) => Promise<GenPixOutput>;
+
+	refund: (i: RefundInput) => Promise<void>;
+}
